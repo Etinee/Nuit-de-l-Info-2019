7a9b19e37f9e898b41a275fad75cbeb9a9d9c896
Lambert Timothée <timothee.lambert@etu.univ-lyon1.fr>
2019-12-05 18:21:58 +0100

    Ajout exemple de test JS

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "ready!" );
+    $("#btn_exemple").click(function(){
+    	$("#exemple").append("<h1>Ceci est un test</h1>");
+    });
 });
