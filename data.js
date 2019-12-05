33a00b88e6ce2fb12180ea5331a38364b0048db4
Lambert Timothée <timothee.lambert@etu.univ-lyon1.fr>
2019-12-05 18:40:30 +0100

    init projet 2

--- data.js
+++ data.js
@@ -1,6 +1,14 @@
 var test = 
 {
-	"data": "salut"
+	"data": "salut",
+	"data2": "salut",
+	"data3": "salut",
+	"data4": "salut",
+	"data5": "salut",
+	"data6": "salut",
+	"data7": "salut",
+	"data8": "salut",
+	"data9": "salut"
 };
 
 console.log(test);
