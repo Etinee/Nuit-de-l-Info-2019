bceb77d679068ccfc2459a999897c6260a579e67
Lambert Timothée <timothee.lambert@etu.univ-lyon1.fr>
2019-12-06 01:09:30 +0100

    pull

--- script.js
+++ script.js
@@ -1,8 +1,72 @@
 // A $( document ).ready() block.
+function objectMove(th){
+	let larg = th.width();
+    let haut = th.height();
+    let mouseX = event.pageX;
+    let mouseY = event.pageY;
+    console.log("th",th," Larg: ",larg," haut: ",haut," mouseX: ",mouseX," mouseY: ",mouseY);
+}
+
 $( document ).ready(function() {
-    console.log( "ready!" );
-    $("#btn_exemple").click(function(){
-    	for(let i = 0; i<50; i++)
-    		$("#exemple").append("<h1>Ceci est un test</h1>");
-    });
+    $("#exemple").draggit(".oui");
 });
+
+jQuery.fn.draggit = function (el) {
+    var thisdiv = this;
+    var thistarget = $(el);
+    var relX;
+    var relY;
+    var targetw = thistarget.width();
+    var targeth = thistarget.height();
+    var docw;
+    var doch;
+    var ismousedown = false;
+    thistarget.css('position','absolute');
+
+
+    thistarget.bind('mousedown', function(e){
+        var pos = $(el).offset();
+        var srcX = pos.left;
+        var srcY = pos.top;
+
+        docw = $('body').width();
+        doch = $('body').height();
+
+        relX = e.pageX - srcX;
+        relY = e.pageY - srcY;
+
+        ismousedown = true;
+    });
+
+    $(document).bind('mousemove',function(e){ 
+        if(ismousedown)
+        {
+            targetw = thistarget.width();
+            targeth = thistarget.height();
+
+            var maxX = docw - targetw - 10;
+            var maxY = doch - targeth - 10;
+
+            var mouseX = e.pageX;
+            var mouseY = e.pageY;
+
+            var diffX = mouseX - relX;
+            var diffY = mouseY - relY;
+
+            // check if we are beyond document bounds ...
+            if(diffX < 0)   diffX = 0;
+            if(diffY < 0)   diffY = 0;
+            if(diffX > maxX) diffX = maxX;
+            if(diffY > maxY) diffY = maxY;
+
+            $(el).css('top', (diffY)+'px');
+            $(el).css('left', (diffX)+'px');
+        }
+    });
+
+    $(window).bind('mouseup', function(e){
+        ismousedown = false;
+    });
+
+    return this;
+} // end jQuery draggit function //
